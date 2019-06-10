b8fe3ad73abd1fe720560dc09558dc088bb93c0a
Miha Pleško <miha.plesko@xlab.si>
2019-06-10 09:20:39 +0200

    Move EMS factory from core repo to plugin repo
    With this commit we move the factory which was originally meant to
    reside in core repo into our base repo.
    
    Signed-off-by: Miha Pleško <miha.plesko@xlab.si>

--- spec/factories/ext_management_system.rb
+++ spec/factories/ext_management_system.rb
@@ -1,4 +1,18 @@
 FactoryBot.define do
+  factory :ems_azure_stack,
+          :aliases => ["manageiq/providers/azure_stack/cloud_manager"],
+          :class   => "ManageIQ::Providers::AzureStack::CloudManager",
+          :parent  => :ems_cloud
+
+  factory :ems_azure_stack_with_authentication,
+          :parent => :ems_azure_stack do
+    azure_tenant_id { "ABCDEFGHIJABCDEFGHIJ0123456789AB" }
+    subscription { "0123456789ABCDEFGHIJABCDEFGHIJKL" }
+    after :create do |x|
+      x.authentications << FactoryBot.create(:authentication)
+    end
+  end
+
   factory :ems_azure_stack_with_vcr_authentication, :parent => :ems_azure_stack do
     zone do
       _guid, _server, zone = EvmSpecHelper.create_guid_miq_server_zone
