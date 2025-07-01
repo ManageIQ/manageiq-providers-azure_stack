554d9926b360077dea4516a9a7c291c7be04930e
Jason Frey <fryguy9@gmail.com>
2025-07-01 15:35:38 -0400

    Switch to use VcrSecrets helper from core

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,29 +11,15 @@ require "manageiq/providers/azure_stack"
 VCR.configure do |config|
   config.ignore_hosts 'codeclimate.com' if ENV['CI']
   config.cassette_library_dir = File.join(ManageIQ::Providers::AzureStack::Engine.root, 'spec/vcr_cassettes')
-
+  config.default_cassette_options = { :match_requests_on => [:method, :path] }
   config.before_playback do |interaction|
     interaction.filter!('AZURE_STACK_HOST', 'azure_stack_host')
   end
-  config.default_cassette_options = { :match_requests_on => [:method, :path] }
   config.filter_sensitive_data('AZURE_STACK_TOKEN') do |interaction|
     if (auth_header = interaction.request.headers['Authorization'])
       auth_header.first.sub(/^Bearer /, '')
     end
   end
-  config.define_cassette_placeholder(Rails.application.secrets.azure_stack_defaults[:host]) do
-    Rails.application.secrets.azure_stack[:host]
-  end
-  config.define_cassette_placeholder(Rails.application.secrets.azure_stack_defaults[:tenant]) do
-    Rails.application.secrets.azure_stack[:tenant]
-  end
-  config.define_cassette_placeholder(Rails.application.secrets.azure_stack_defaults[:subscription]) do
-    Rails.application.secrets.azure_stack[:subscription]
-  end
-  config.define_cassette_placeholder(Rails.application.secrets.azure_stack_defaults[:userid]) do
-    Rails.application.secrets.azure_stack[:userid]
-  end
-  config.define_cassette_placeholder(Rails.application.secrets.azure_stack_defaults[:password]) do
-    Rails.application.secrets.azure_stack[:password]
-  end
+
+  VcrSecrets.define_all_cassette_placeholders(config, :azure_stack)
 end
